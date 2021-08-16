9114c1b1030faa1b6d5e19b0e516aae615e6295b
Alex Schearer <aschearer@gmail.com>
2021-08-15 21:25:41 -0700

    Adding unity events for Color, float.

---
+++ Scripts/Extensions/ColorUnityEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace SpottedZebra.UnityFoundation.Extensions
+{
+    [Serializable]
+    public sealed class ColorUnityEvent : UnityEvent<Color>
+    {
+        
+    }
+}

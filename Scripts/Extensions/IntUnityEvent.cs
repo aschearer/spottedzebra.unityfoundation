131c766416ee4e32ae2fa6296e0ff21783262fa9
Alex Schearer <aschearer@gmail.com>
2021-08-16 00:50:07 -0700

    Adding Unity int event.

---
+++ Scripts/Extensions/IntUnityEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine.Events;
+
+namespace SpottedZebra.UnityFoundation.Extensions
+{
+    [Serializable]
+    public sealed class IntUnityEvent : UnityEvent<int>
+    {
+        
+    }
+}

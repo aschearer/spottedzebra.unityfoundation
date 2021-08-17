728f6a3936a8e1b1cd8a5c2eb38a599b43e0ef45
Alex Schearer <aschearer@gmail.com>
2021-08-17 09:53:47 -0700

    Adding constructors for FloatReference

--- Scripts/Variables/FloatReference.cs
+++ Scripts/Variables/FloatReference.cs
@@ -5,5 +5,15 @@ namespace SpottedZebra.UnityFoundation.Variables
     [Serializable]
     public sealed class FloatReference : VariableReferenceBase<FloatVariable, float>
     {
+        public FloatReference()
+            : this(0)
+        {
+        }
+        
+        public FloatReference(float value)
+        {
+            this.UseConstant = true;
+            this.SetValue(value);
+        }
     }
 }

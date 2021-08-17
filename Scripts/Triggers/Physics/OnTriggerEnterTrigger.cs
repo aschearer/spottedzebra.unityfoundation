db4b8d0caea476f0c7744ebf70a73b61786eaddb
Alex Schearer <aschearer@gmail.com>
2021-08-17 09:53:37 -0700

    Adding triggers for physics trigger enter/exit

---
+++ Scripts/Triggers/Physics/OnTriggerEnterTrigger.cs
@@ -0,0 +1,23 @@
+﻿using Sirenix.OdinInspector;
+using SpottedZebra.UnityFoundation.Extensions;
+using UnityEngine;
+
+namespace SpottedZebra.UnityFoundation.Triggers.Physics
+{
+    public sealed class OnTriggerEnterTrigger : FoundationTriggerBase
+    {
+        [SerializeField] private bool filterByTag;
+
+        [ShowIf("filterByTag")] [TagSelector] [SerializeField]
+        private string targetTag;
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!this.filterByTag ||
+                other.CompareTag(this.targetTag))
+            {
+                this.Trigger();
+            }
+        }
+    }
+}
---
+++ Scripts/Triggers/Physics/OnTriggerExitTrigger.cs
@@ -0,0 +1,23 @@
+﻿using Sirenix.OdinInspector;
+using SpottedZebra.UnityFoundation.Extensions;
+using UnityEngine;
+
+namespace SpottedZebra.UnityFoundation.Triggers.Physics
+{
+    public sealed class OnTriggerExitTrigger : FoundationTriggerBase
+    {
+        [SerializeField] private bool filterByTag;
+
+        [ShowIf("filterByTag")] [TagSelector] [SerializeField]
+        private string targetTag;
+        
+        private void OnTriggerExit(Collider other)
+        {
+            if (!this.filterByTag ||
+                other.CompareTag(this.targetTag))
+            {
+                this.Trigger();
+            }
+        }
+    }
+}
